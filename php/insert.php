3673c26fb1c8a19954f175cc089e2bab0013d28b
anxxt <antonio.garcia-garcia@outlook.com>
2021-06-16 15:39:52 +0200

    añadido php que sirve para insertar nuestros datos recibidos en la bbdd

---
+++ php/insert.php
@@ -0,0 +1,27 @@
+<?php
+require_once('config/config.php');
+
+$data_json = json_decode(file_get_contents('php://input'), true);
+
+$data = $data_json['data'];
+$epoch = time();
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO matriculas (epoch, matricula) VALUES ('$epoch', '$data')";
+
+// Perform a query, check for error
+if (!mysqli_query($conn, $sql)) {
+    die("Error description: " . mysqli_error($conn));
+}
+
+echo "Insertado";
+
+mysqli_close($conn);
+
+?>
