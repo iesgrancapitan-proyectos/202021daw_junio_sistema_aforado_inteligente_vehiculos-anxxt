bc384c545a7efb6c5c3b63cef816bcd5f703884d
anxxt <antonio.garcia-garcia@outlook.com>
2021-06-20 20:00:44 +0200

    añadido insert a interfaz_web

---
+++ interfaz_web/insert.php
@@ -0,0 +1,29 @@
+<?php
+require_once('config/config.php');
+
+$data_json = json_decode(file_get_contents('php://input'), true);
+
+if ($data_json == null) exit(1);
+
+$data = $data_json['data'];
+$epoch = time() + 7200; // 7200 le añade 2 horas que es nuetro timezone
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO matriculas (epoch, matricula) VALUES ('$epoch', '$data')";
+
+// Perform a query, check for error
+if (!mysqli_query($conn, $sql)) {
+    die("Error description: " . mysqli_error($conn));
+}
+
+echo "Insertado";
+
+mysqli_close($conn);
+
+?>
